941b50cf7abfce220f023046c53f5f7e01d0bea4
tukuyo <ht15a053@oecu.jp>
2020-07-28 23:29:26 +0900

    Initial Commit

---
+++ SwiftUI_Map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Map
+//
+//  Created by Yoshio on 2020/07/28.
+//  Copyright © 2020 tukuyo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
