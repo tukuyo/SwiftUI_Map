4a708be9c0fcfcd807a2fb7e364840b19b9211a1
tukuyo <ht15a053@oecu.jp>
2020-07-30 23:52:41 +0900

    part 2

--- SwiftUI_Map/MapView.swift
+++ SwiftUI_Map/MapView.swift
@@ -12,29 +12,49 @@ import MapKit
 
 struct MapView: UIViewRepresentable {
     
+    
     func makeUIView(context: Context) -> MKMapView {
-        return MKMapView(frame: .zero)
+        let map = MKMapView()
+        map.delegate = context.coordinator
+        map.addGestureRecognizer(context.coordinator.myLongPress)
+        return map
     }
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
      // ビューの更新時に呼ばれる
     func updateUIView(_ mapView: MKMapView, context: Context) {
-        // 緯度経度を設定
-        let latitude = 34.011286, longitude = -116.166868
-        let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
-        // 縮尺を設定
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
-        // マップの中心を設定
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        mapView.setRegion(region, animated: true)
+    }
 
-        // pinを立てる
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = coordinate  // ピンを立てる位置設定
-        annotation.title = "Hello, World"   // ピンのタイトル設定
-        annotation.subtitle = "hoge"        // タップしたときの詳細設定
-        mapView.addAnnotation(annotation)   // ピンをマップビューに追加
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        let myLongPress: UILongPressGestureRecognizer = UILongPressGestureRecognizer()
 
-
+        init(_ parent: MapView) {
+            self.parent = parent
+            super.init()
+            
+            self.myLongPress.addTarget(self, action: #selector(recognizeLongPress))
+            
+        }
+        
+        @objc func recognizeLongPress(sender: UILongPressGestureRecognizer) {
+            if sender.state == .ended {
+                if let mapView = sender.view as? MKMapView {
+                    // タップした位置を取得
+                    let point = sender.location(in: mapView)
+                    // mapView上での位置に変換
+                    let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+                    // アノテーション作成
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = coordinate
+                    mapView.addAnnotation(annotation)
+                }
+            }
+        }
     }
 }
 
